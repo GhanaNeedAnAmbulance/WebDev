00d781e02b3100905eb127c1e9738d38a4f8a8d8
stephen-hannon <stephenhannon4@gmail.com>
2018-11-03 13:46:31 -0400

    Adds event listeners to each hospital's beds

--- database.js
+++ database.js
@@ -1,8 +1,18 @@
-firebase.database().ref('/Hospital').on('value', function (snapshot) {
-    console.log(snapshot.val());
+firebase.database().ref('/Hospital').once('value', function (snapshot) {
+    console.log(Object.keys(snapshot.val()));
     app.loading = false;
     app.hospitals = snapshot.val();
     app.snapshot = snapshot
     if (app.markers.length == 0) // if markers already loaded, don't do again
-        app.loadMarker()
-})
+        app.loadMarker();
+
+    addBedListeners(snapshot.val());
+});
+
+function addBedListeners (data) {
+    Object.keys(data).forEach(function (hospital) {
+        firebase.database().ref('/Hospital/' + hospital + '/emptyBeds').on('value', function (snapshot) {
+            app.hospitals[hospital].emptyBeds = snapshot.val();
+        })
+    })
+}
