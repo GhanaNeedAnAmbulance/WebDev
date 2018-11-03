77d9095f12f757ea9475900724bdfa9858480598
stephen-hannon <stephenhannon4@gmail.com>
2018-11-03 15:01:26 -0400

    CSS/responsive improvements

--- database.js
+++ database.js
@@ -1,10 +1,8 @@
 firebase.database().ref('/Hospital').once('value', function (snapshot) {
-    console.log(Object.keys(snapshot.val()));
     app.loading = false;
     app.hospitals = snapshot.val();
     app.snapshot = snapshot
-    if (app.markers.length == 0) // if markers already loaded, don't do again
-        app.loadMarker();
+    app.loadMarker();
 
     addBedListeners(snapshot.val());
 });
