e3d4e73c75268579f71265591bcb90b2be561456
Rayyan Karim <rkarim0598@gmail.com>
2018-11-03 00:56:57 -0400

    Add map object to global object and remove excess firebase code

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     var app = new Vue({
         el: '#app',
         data: {
-            hospitalName: 'Hospital Name!',
-            address: '123 Sesame St.',
-            phone: '123-456-7890'
+            hospitalData: {
+                hospitalName: 'Hospital Name!',
+                address: '123 Sesame St.',
+                phone: '123-456-7890',
+            },
+            mapName: 'My map'
         }
     });
 });
