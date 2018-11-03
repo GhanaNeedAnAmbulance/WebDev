5a83587ed66800080e3251f30cca6eac99c6c549
Rayyan Karim <rkarim0598@gmail.com>
2018-11-03 12:51:05 -0400

    Add markers to map

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ var app = new Vue({
     data: {
         loading: true,
         hospitals: {},
-        markers: []
+        snapshot: {},
+        markers: [],
+        map: null
     },
     mounted: function () { // initialize map after component loaded
 
-        this.map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 61.180059, lng: -149.822075 },
             scrollwheel: false,
             zoom: 4
         })
+
     },
-    
+    methods: {
+        loadMarker: function(markerData) {
+            this.snapshot.forEach(function(child) {
+                var childs = child.val()
+                var marker = new google.maps.Marker({
+                    position: {lat: childs.lat, lng: childs.lng},
+                    map: this.map
+                })
+            })
+        }
+    }
 });
--- database.js
+++ database.js
@@ -2,4 +2,6 @@ firebase.database().ref('/Hospital').on('value', function (snapshot) {
     console.log(snapshot.val());
     app.loading = false;
     app.hospitals = snapshot.val();
+    app.snapshot = snapshot
+    app.loadMarker()    
 })
