036ce921af771e30ed3133d5774c80818cb35792
stephen-hannon <stephenhannon4@gmail.com>
2018-11-02 22:51:18 -0400

    Adds boilerplate with Vue and Material

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+    var app = new Vue({
+        el: '#app',
+        data: {
+            hospitalName: 'Hospital Name!',
+            address: '123 Sesame St.',
+            phone: '123-456-7890'
+        }
+    });
+});
