031f7a4e9272ca51af5d7d0d9faea6e513b53860
Rayyan Karim <rkarim0598@gmail.com>
2018-11-03 13:28:05 -0400

    Move map center

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ var app = new Vue({
     mounted: function () { // initialize map after component loaded
 
         map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 61.180059, lng: -149.822075 },
+            center: { lat: 41.7056, lng: -86.2353 },
             scrollwheel: false,
             zoom: 4
         })
 
     },
     methods: {
-        loadMarker: function(markerData) {
-            this.snapshot.forEach(function(child) {
+        loadMarker: function (markerData) {
+            this.snapshot.forEach(function (child) {
                 var childs = child.val()
                 var marker = new google.maps.Marker({
-                    position: {lat: childs.lat, lng: childs.lng},
+                    position: { lat: childs.lat, lng: childs.lng },
                     map: this.map
                 })
             })
